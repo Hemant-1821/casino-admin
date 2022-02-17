c44bd0936e8556809a6aedfed39f71f9fb21be1d
Hemant Singh <singh.hemant9583@gmail.com>
2022-02-18 00:31:28 +0530

    Update Withdraw.jsx

--- src/Withdraw.jsx
+++ src/Withdraw.jsx
@@ -120,82 +120,62 @@ function Withdraw() {
           <div className="py-3 card margin-top container bg-dark trading-card text-white">
             <div className="card-body">
               <div className="h1 text-center">Edit Request Form</div>
-              <div className="row mt-5">
-                <div className="d-flex justify-content-between">
-                  <div className="d-flex">
-                    <p className="h4 mt-1">Amount:</p>
-                    <input
-                      style={{ width: "8rem" }}
-                      type="number"
-                      className="form-control"
-                      aria-describedby="Amount"
-                      value={state.amt}
-                      onChange={(e) => onAmtInput(e)}
-                      disabled
-                    />
-                  </div>
-                  <div className="d-flex">
-                    <p className="h4 me-3 mt-1">Account Number:</p>
-                    <input
-                      style={{ width: "18rem" }}
-                      type="number"
-                      className="form-control"
-                      aria-describedby="Amount"
-                      value={state.accountNo}
-                      onChange={(e) => onAmtInput(e)}
-                      disabled
-                    />
-                  </div>
-                </div>
-                <div className="d-flex justify-content-around mt-5">
-                  <div className="d-flex">
-                    <p className="h4 mt-1">IFSC Code:</p>
-                    <input
-                      style={{ width: "10rem" }}
-                      type="text"
-                      className="form-control"
-                      aria-describedby="Amount"
-                      value={state.ifsc}
-                      onChange={(e) => onAmtInput(e)}
-                      disabled
-                    />
-                  </div>
-                  <div className="d-flex">
-                    <p className="h4 mt-1">Amount Transferred:</p>
-                    <input
-                      style={{ width: "10rem" }}
-                      type="text"
-                      className="form-control"
-                      id="amtTrans"
-                      aria-describedby="Amount"
-                      value={state.amtTrans}
-                      onChange={(e) => onAmtInput(e)}
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-6 d-flex mt-3">
-                    <p className="h4 mt-1">Transaction Id:</p>
-                    <input
-                      style={{ width: "10rem" }}
-                      type="text"
-                      className="form-control"
-                      id="transactionId"
-                      aria-describedby="Amount"
-                      value={state.transactionId}
-                      onChange={(e) => onAmtInput(e)}
-                    />
-                  </div>
-                  <div className="col-6 mt-3">
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      onClick={onSubmit}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </div>
+              <div
+                className="row mt-5"
+                style={{ paddingLeft: "25%", paddingRight: "25%" }}
+              >
+                <p className="mt-1 mb-0">Amount:</p>
+                <input
+                  type="number"
+                  className="form-control"
+                  aria-describedby="Amount"
+                  value={state.amt}
+                  onChange={(e) => onAmtInput(e)}
+                  disabled
+                />
+                <p className="mt-1 mb-0">Account Number:</p>
+                <input
+                  type="number"
+                  className="form-control"
+                  aria-describedby="Amount"
+                  value={state.accountNo}
+                  onChange={(e) => onAmtInput(e)}
+                  disabled
+                />
+                <p className="mt-1 mb-0">IFSC Code:</p>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-describedby="Amount"
+                  value={state.ifsc}
+                  onChange={(e) => onAmtInput(e)}
+                  disabled
+                />{" "}
+                <p className="mt-1 mb-0">Amount Transferred:</p>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="amtTrans"
+                  aria-describedby="Amount"
+                  value={state.amtTrans}
+                  onChange={(e) => onAmtInput(e)}
+                />
+                <p className="mt-1 mb-0">Transaction Id:</p>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="transactionId"
+                  aria-describedby="Amount"
+                  value={state.transactionId}
+                  onChange={(e) => onAmtInput(e)}
+                />
+                <button
+                  type="button"
+                  className="btn btn-primary mt-3"
+                  onClick={onSubmit}
+                >
+                  Submit
+                </button>
               </div>
             </div>
           </div>
