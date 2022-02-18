864b562e4b0619f78d00fa706dbdc818a12c7e35
Hemant Singh <singh.hemant9583@gmail.com>
2022-02-18 12:42:49 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,9 +7,17 @@ const Home = () => {
     earning: 0,
   };
   const [state, setState] = useState(initialState);
+  const [earn, setEarn] = useState(0);
   useEffect(async () => {
     await axiosInstance.get("/user/count").then((resp) => {
       setState({ ...state, userCount: resp.data.count });
+    });
+    await axiosInstance.get("/admin/earnings").then((resp) => {
+      let totalEarn = 0;
+      resp.data.requests.forEach((req) => {
+        totalEarn = +totalEarn + +req.amt;
+      });
+      setEarn(totalEarn);
     });
   });
   return (
@@ -23,6 +31,10 @@ const Home = () => {
           <div className="d-flex">
             <div className="h4">Registered Users:&nbsp;&nbsp;</div>
             <div className="h4">{`  ${state.userCount}`}</div>
+          </div>
+          <div className="d-flex">
+            <div className="h4">Total Earnings:&nbsp;&nbsp;</div>
+            <div className="h4">{`  ${earn}`}</div>
           </div>
         </div>
       </div>
